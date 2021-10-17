fc25e90e17e81c6efcccf72baaf76f07b4e8df05
Sakamoto, Kazunori <exkazuu@gmail.com>
2021-10-17 09:42:34 +0900

    fix!: simplify compressJson and compressJsonText

--- src/jsonCompressor.ts
+++ src/jsonCompressor.ts
@@ -2,16 +2,14 @@ import fs from 'fs';
 
 import { gunzipSync, gzipSync, strFromU8, strToU8 } from 'fflate';
 
-export function compressJson(obj: unknown, ...filePaths: string[]): void {
-  compressJsonText(JSON.stringify(obj), ...filePaths);
+export function compressJson(obj: unknown, filePath: string): void {
+  compressJsonText(JSON.stringify(obj), filePath);
 }
 
-export function compressJsonText(jsonText: string, ...filePaths: string[]): void {
+export function compressJsonText(jsonText: string, filePath: string): void {
   const buf = strToU8(jsonText);
   const compressed = gzipSync(buf);
-  for (const filePath of filePaths) {
-    fs.writeFileSync(filePath, compressed);
-  }
+  fs.writeFileSync(filePath, compressed);
 }
 
 export function decompressJson(filePath: string): any {
