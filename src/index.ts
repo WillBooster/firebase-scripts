4ba3ed7c5eb2615160d7cacf809e50f685ed98ef
Sakamoto, Kazunori <exkazuu@gmail.com>
2021-10-19 13:21:54 +0900

    feat: expose more functions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 export { exportCollections, exportCollection } from './exportCommand';
 export { importCollection, restoreCollection, convertObjectToTimestamp } from './importCommand';
-export { compressJson, compressJsonText, decompressJson } from './jsonCompressor';
+export {
+  compressJson,
+  compressJsonText,
+  decompressJson,
+  decompressJsonText,
+  getFormatFromExtension,
+  getExtensionFromFormat,
+} from './jsonCompressor';
