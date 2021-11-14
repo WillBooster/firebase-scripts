00ccae079791858a4f8e7f6b4c297c022920e8a1
remin <rdrgn@users.noreply.github.com>
2021-11-15 08:40:40 +0900

    feat: rename options of the copy command

--- src/copyCommand.ts
+++ src/copyCommand.ts
@@ -4,23 +4,14 @@ import type { CommandModule, InferredOptionTypes } from 'yargs';
 import { initializeAdmin } from './firebaseAdmin';
 
 const builder = {
-  dest: {
-    description: 'A path of the destination document. If not provided, the same path as the source will be used.',
-    type: 'string',
-  },
-  destCred: {
-    description:
-      'A service account file to write the destination document. If not provided, the same service account as the source will be used.',
-    type: 'string',
-  },
-  src: {
-    demandOption: true,
-    description: 'A path of the source document.',
-    type: 'string',
-  },
-  srcCred: {
+  'source-service-account-path': {
     demandOption: true,
     description: 'A service account file to read the source document.',
+    type: 'string',
+  },
+  'target-service-account-path': {
+    description:
+      'A service account file to write the target document. If not provided, the same service account as the source will be used.',
     type: 'string',
   },
 } as const;
@@ -30,26 +21,42 @@ export const copyCommand: CommandModule<unknown, InferredOptionTypes<typeof buil
   describe: 'Copy a document',
   builder,
   async handler(argv) {
-    const srcAdminApp = initializeAdmin({ name: 'src', serviceAccountPathOrObject: argv.srcCred });
-    const destAdminApp = initializeAdmin({ name: 'dest', serviceAccountPathOrObject: argv.destCred ?? argv.srcCred });
-    await copyDocument(srcAdminApp, argv.src, destAdminApp, argv.dest ?? argv.src);
+    if (argv._.length < 1) {
+      console.error('Provide the document path to copy.');
+      process.exit(1);
+    }
+
+    const source = argv._[0].toString();
+    const target = argv._[1] !== undefined ? argv._[1].toString() : source;
+
+    const srcAdminApp = initializeAdmin({
+      name: 'source',
+      serviceAccountPathOrObject: argv['source-service-account-path'],
+    });
+
+    const destAdminApp = initializeAdmin({
+      name: 'dest',
+      serviceAccountPathOrObject: argv['target-service-account-path'] ?? argv['source-service-account-path'],
+    });
+
+    await copyDocument(srcAdminApp, source, destAdminApp, target);
   },
 };
 
 export async function copyDocument(
-  srcAdminApp: app.App,
-  srcDocumentPath: string,
-  destAdminApp: app.App,
-  destDocumentPath: string
+  sourceAdminApp: app.App,
+  sourceDocumentPath: string,
+  targetAdminApp: app.App,
+  targetDocumentPath: string
 ): Promise<void> {
-  console.info(`Reading the source document '${srcDocumentPath}'...`);
-  const doc = (await srcAdminApp.firestore().doc(srcDocumentPath).get()).data();
+  console.info(`Reading the source document '${sourceDocumentPath}'...`);
+  const doc = (await sourceAdminApp.firestore().doc(sourceDocumentPath).get()).data();
 
   if (!doc) {
-    console.error(`The source document '${srcDocumentPath}' does not exist.`);
+    console.error(`The source document '${sourceDocumentPath}' does not exist.`);
     return;
   }
 
-  console.info(`Writing the destination document '${destDocumentPath}'...'`);
-  destAdminApp.firestore().doc(destDocumentPath).set(doc);
+  console.info(`Writing the target document '${targetDocumentPath}'...'`);
+  targetAdminApp.firestore().doc(targetDocumentPath).set(doc);
 }
