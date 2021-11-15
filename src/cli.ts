512fbdcd654dd05aa7fb6a0695ac71523e86db2e
remin <rdrgn@users.noreply.github.com>
2021-11-15 11:51:30 +0900

    feat: a command to download a document or a field

--- src/cli.ts
+++ src/cli.ts
@@ -1,8 +1,9 @@
 import yargs from 'yargs/yargs';
 
+import { downloadCommand } from './downloadCommand';
 import { exportCommand } from './exportCommand';
 import { importCommand } from './importCommand';
 
 export async function main(): Promise<void> {
-  await yargs(process.argv.slice(2)).command(importCommand).command(exportCommand).help().argv;
+  await yargs(process.argv.slice(2)).command(downloadCommand).command(importCommand).command(exportCommand).help().argv;
 }
