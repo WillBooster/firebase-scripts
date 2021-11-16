c32f6dc15c967849dace9898c4204120b70aa795
remin <rdrgn@users.noreply.github.com>
2021-11-16 14:39:24 +0900

    build: esbuild (#21)
    * build: esbuild
    
    * build: remove Rollup and Babel
    
    * test: create a Jest transformer to use esbuild
    
    * build: add esbuild as a dev dependency

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,9 @@
-import yargs from 'yargs/yargs';
-
 import { exportCommand } from './exportCommand';
 import { importCommand } from './importCommand';
+
+// https://github.com/yargs/yargs/issues/1929#issuecomment-920391458
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const yargs = require('yargs');
 
 export async function main(): Promise<void> {
   await yargs(process.argv.slice(2)).command(importCommand).command(exportCommand).help().argv;
