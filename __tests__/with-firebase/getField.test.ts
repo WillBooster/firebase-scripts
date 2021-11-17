51012d7c84ca95dc8bc3694bc7b6a71865ea5ea1
remin <rdrgn@users.noreply.github.com>
2021-11-17 13:37:35 +0900

    test: getField error cases

--- __tests__/with-firebase/getField.test.ts
+++ __tests__/with-firebase/getField.test.ts
@@ -9,25 +9,51 @@ const adminApp = initializeAdmin();
 
 type Case = [string, string, FirebaseFirestore.DocumentData, any];
 
-test.each<Case>([
-  ['collection/document', 'field', { field: 'string' }, 'string'],
-  [
-    'collection/document',
-    'field',
-    { field: Buffer.from([0x62, 0x75, 0x66, 0x66, 0x65, 0x72]) },
-    Buffer.from([0x62, 0x75, 0x66, 0x66, 0x65, 0x72]),
-  ],
-  [
-    'collection/document/collection/document',
-    'field1.field2.field3.field4',
-    { field1: { field2: { field3: { field4: 1 } } } },
-    1,
-  ],
-])('getField(%j, %j, %j, %j)', async (documentPath, fieldPath, document, expectedField) => {
-  // Prepare a document for testing.
-  await adminApp.firestore().doc(documentPath).set(document);
+describe('getField', () => {
+  test.each<Case>([
+    ['collection/document', 'field', { field: 'string' }, 'string'],
+    [
+      'collection/document',
+      'field',
+      { field: Buffer.from([0x62, 0x75, 0x66, 0x66, 0x65, 0x72]) },
+      Buffer.from([0x62, 0x75, 0x66, 0x66, 0x65, 0x72]),
+    ],
+    [
+      'collection/document/collection/document',
+      'field1.field2.field3.field4',
+      { field1: { field2: { field3: { field4: 1 } } } },
+      1,
+    ],
+  ])('(%j, %j, %j, %j)', async (documentPath, fieldPath, document, expectedField) => {
+    // Prepare a document for testing.
+    await adminApp.firestore().doc(documentPath).set(document);
 
-  // Test.
-  const field = await getField(adminApp, documentPath, fieldPath);
-  expect(field).toEqual(expectedField);
+    // Test.
+    const field = await getField(adminApp, documentPath, fieldPath);
+    expect(field).toEqual(expectedField);
+  });
+
+  test('non-existent collection', async () => {
+    await adminApp.firestore().doc('collection/document').set({ field: 'value' });
+
+    await expect(() => getField(adminApp, 'nonExistentCollection/document', 'field')).rejects.toThrow(
+      'The document "nonExistentCollection/document" does not exist.'
+    );
+  });
+
+  test('non-existent document', async () => {
+    await adminApp.firestore().doc('collection/document').set({ field: 'value' });
+
+    await expect(() => getField(adminApp, 'collection/nonExistentDocument', 'field')).rejects.toThrow(
+      'The document "collection/nonExistentDocument" does not exist.'
+    );
+  });
+
+  test('non-existent field', async () => {
+    await adminApp.firestore().doc('collection/document').set({ field: 'value' });
+
+    await expect(() => getField(adminApp, 'collection/document', 'nonExistentField')).rejects.toThrow(
+      'The field "nonExistentField" does not exist in the document "collection/document".'
+    );
+  });
 });
