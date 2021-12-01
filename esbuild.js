f7ea10073d547b0c5e68abc4c22d8392c5b57dfb
Sakamoto, Kazunori <exkazuu@gmail.com>
2021-12-02 01:52:14 +0900

    fix: don't bundle dependencies

--- esbuild.js
+++ esbuild.js
@@ -1,10 +1,23 @@
+const { builtinModules } = require('module');
+const path = require('path');
+
 const { build } = require('esbuild');
 const { dtsPlugin } = require('esbuild-plugin-d.ts');
+
+const package = require(path.resolve('package.json'));
+
+const external = [
+  ...builtinModules,
+  ...Object.keys(package.dependencies ?? {}),
+  ...Object.keys(package.devDependencies ?? {}),
+  ...Object.keys(package.peerDependencies ?? {}),
+];
 
 Promise.all([
   build({
     bundle: true,
     entryPoints: ['src/index.ts'],
+    external,
     format: 'esm',
     minify: true,
     outfile: 'dist/index.min.mjs',
@@ -15,6 +28,7 @@ Promise.all([
   build({
     bundle: true,
     entryPoints: ['src/index.ts'],
+    external,
     format: 'cjs',
     minify: true,
     outfile: 'dist/index.min.cjs',
@@ -24,6 +38,7 @@ Promise.all([
   build({
     bundle: true,
     entryPoints: ['src/cli.ts'],
+    external,
     minify: true,
     outfile: 'dist/cli.min.cjs',
     platform: 'node',
